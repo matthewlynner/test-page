54dbd657f74cdd8a8392c617980033e488f9ce9e
Matthew Lynner <matthewlynner@Matthews-MacBook-Pro.local>
2018-12-31 14:26:53 -0600

    javascript lesson start

--- main.js
+++
@@ -1,5 +0,0 @@
-function myFunction() {
-    document.getElementById("greeting").innerHTML = "Hi christina";
-    document.getElementById("funny").innerHTML = "You have nice buns..";
-    document.getElementsByClassName("christina");
-  }
